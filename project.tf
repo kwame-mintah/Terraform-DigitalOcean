961f7e3dfa88dd8e5c863abf40c2b5cc7438a4f6
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2022-03-13 17:47:04 +0000

    feat(project): create project to organize resources
    will create a new project in your digitalocean account. to help with grouping resources like droplets, loadbalancers etc.

---
+++ project.tf
@@ -0,0 +1,16 @@
+# Create a new project rather than using default. This will make it easier to organize 
+# resources into groups.
+resource "digitalocean_project" "project" {
+  name        = var.environment
+  description = "A project to represent resources for an environment."
+  purpose     = "Project resources for ${var.environment}"
+  environment = var.environment
+  resources = [
+    digitalocean_droplet.droplet.*.urn[0],
+    digitalocean_loadbalancer.droplet.urn
+  ]
+}
+# Create a new tag to be used on droplets
+resource "digitalocean_tag" "tags" {
+  name = var.environment
+}
